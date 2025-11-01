bd2268b39a06824176b794263ec3e17991b29c13
morena1001 <josueflores15.jf@gmail.com>
2025-11-01 13:16:22 -0500

    updating library files

--- ADM0802A.c
+++ ADM0802A.c
@@ -8,12 +8,77 @@
 #include "ADM0802A.h"
 
 static uint8_t CharToEncoding (char character, bool upper_set);
+static GPIO_TypeDef* GetPort (char* port);
+static uint16_t GetPin (char* pin);
 
 /*
  * INITIALIZATION
  */
 
-uint8_t ADM0802A_Init () {
+uint8_t ADM0802A_Init (char* rw_pair, char* rs_pair, char* ca_pair, char* e_pair, char* db0_pair, char* db1_pair, char* db2_pair, char* db3_pair, char* db4_pair, char* db5_pair, char* db6_pair, char* db7_pair) {
+	char* pin = (char*) malloc (2 * sizeof (char));
+
+	// Set rw pin
+	strncpy (pin, rw_pair + 1, 2);
+	port_rw = GetPort (rw_pair);
+	pin_rw = GetPin (pin);
+
+	// Set rs pin
+	strncpy (pin, rs_pair + 1, 2);
+	port_rs = GetPort (rs_pair);
+	pin_rs = GetPin (pin);
+
+	// Set ca pin
+	strncpy (pin, ca_pair + 1, 2);
+	port_ca = GetPort (ca_pair);
+	pin_ca = GetPin (pin);
+
+	// Set e pin
+	strncpy (pin, e_pair + 1, 2);
+	port_e = GetPort (e_pair);
+	pin_e = GetPin (pin);
+
+	// Set db0 pin
+	strncpy (pin, db0_pair + 1, 2);
+	port_db0 = GetPort (db0_pair);
+	pin_db0 = GetPin (pin);
+
+	// Set db1 pin
+	strncpy (pin, db1_pair + 1, 2);
+	port_db1 = GetPort (db1_pair);
+	pin_db1 = GetPin (pin);
+
+	// Set db2 pin
+	strncpy (pin, db2_pair + 1, 2);
+	port_db2 = GetPort (db2_pair);
+	pin_db2 = GetPin (pin);
+
+	// Set db3 pin
+	strncpy (pin, db3_pair + 1, 2);
+	port_db3 = GetPort (db3_pair);
+	pin_db3 = GetPin (pin);
+
+	// Set db4 pin
+	strncpy (pin, db4_pair + 1, 2);
+	port_db4 = GetPort (db4_pair);
+	pin_db4 = GetPin (pin);
+
+	// Set db5 pin
+	strncpy (pin, db5_pair + 1, 2);
+	port_db5 = GetPort (db5_pair);
+	pin_db5 = GetPin (pin);
+
+	// Set db6 pin
+	strncpy (pin, db6_pair + 1, 2);
+	port_db6 = GetPort (db6_pair);
+	pin_db6 = GetPin (pin);
+
+	// Set db7 pin
+	strncpy (pin, db7_pair + 1, 2);
+	port_db7 = GetPort (db7_pair);
+	pin_db7 = GetPin (pin);
+
+
 	// Cursor to move right, DDRAM addr is increased by 1, and no display shift
 	ADM0802A_CommandWrite (ADM0802A_COMMAND_ENTRY_MODE (1, 0));
 
@@ -102,150 +167,145 @@ static inline uint8_t CharToEncoding (char character, bool upper_set) {
 	return (upper_set) ? (0x80 | character) : (character);
 }
 
+static GPIO_TypeDef* GetPort (char* port) {
+	switch (toupper (*port)) {
+		#ifdef GPIOA
+			case 'A':	return PORT (A);
+		#endif
 
-// /*
-//  * ADM0802A.c
-//  *
-//  *  Created on: Feb 22, 2025
-//  *      Author: josue
-//  */
+		#ifdef GPIOB
+			case 'B':	return PORT (B);
+		#endif
 
-// #include "ADM0802A.h"
+		#ifdef GPIOC
+			case 'C':	return PORT (C);
+		#endif
 
-// static uint8_t CharToEncoding (char character, bool upper_set);
+		#ifdef GPIOD
+			case 'D':	return PORT (D);
+		#endif
 
-// /*
-//  * INITIALIZATION
-//  */
+		#ifdef GPIOE
+			case 'E':	return PORT (E);
+		#endif
 
-// uint8_t ADM0802A_Init (uint8_t num_pins_to_set, ...) {
-// 	// //uint8_t ADM0802A_Init (GPIO_TypeDef* RW_port, uint16_t RW_pin);
+		#ifdef GPIOF
+			case 'F':	return PORT (F);
+		#endif
 
-// 	// void Reset_Timer(int num, ...) {
-// 	//	va_list arg_list;
-// 	//	va_start(arg_list, num);
-// 	//
-// 	//	for (int i = 0; i < num; i++) {
-// 	//		TIM_HandleTypeDef* temp = va_arg(arg_list, TIM_HandleTypeDef*);
-// 	//		if (temp->Instance == TIM2) {
-// 	//			HAL_TIM_Base_Stop_IT(&htim2);
-// 	//			start_timer = 0;
-// 	//			HAL_TIM_Base_Start_IT(&htim2);
-// 	//		} else if (temp->Instance == TIM15) {
-// 	//			HAL_TIM_Base_Stop_IT(&htim15);
-// 	//			count = 0;
-// 	//			HAL_TIM_Base_Start_IT(&htim15);
-// 	//		} else if (temp->Instance == TIM16) {
-// 	//			HAL_TIM_Base_Stop_IT(&htim16);
-// 	//			finished_once = 0;
-// 	//			characters_to_move = va_arg(arg_list, int);
-// 	//			HAL_TIM_Base_Start_IT(&htim16);
-// 	//		}
-// 	//	}
-// 	//}
+		#ifdef GPIOG
+			case 'G':	return PORT (G);
+		#endif
 
-// 	va_list arg_list;
-// 	va_start (arg_list, num);
+		#ifdef GPIOH
+			case 'H':	return PORT (H);
+		#endif
 
-// //	switch (num) {
-// //		case 14: {
-// //			GPIO_TypeDef* port = va_arg (arg_list, GPIO_TypeDef*);
-// //			uint16_t pin = va_arg (arg_list, uint16_t);
-// //		}
-// //		default: {
-// //			// Default case for numbers greater than 14
-// //		}
-// //
-// //	}
+		#ifdef GPIOI
+			case 'I':	return PORT (I);
+		#endif
 
-// //	for (int i = 0; i < num; i++) {
-// //		GPIO_TypeDef*
-// //	}
+		#ifdef GPIOJ
+			case 'J':	return PORT (J);
+		#endif
 
-// 	// Cursor to move right, DDRAM addr is increased by 1, and no display shift
-// 	ADM0802A_CommandWrite (ADM0802A_COMMAND_ENTRY_MODE (1, 0));
+		#ifdef GPIOK
+			case 'K':	return PORT (K);
+		#endif
 
-// 	// Turn on display, cursor is enabled, and blink is enabled
-// 	ADM0802A_CommandWrite (ADM0802A_COMMAND_DISPLAY_CONTROL (1, 1, 1));
+		#ifdef GPIOL
+			case 'L':	return PORT (L);
+		#endif
 
-// 	// Cursor and display shift are left off
-// 	// 8-bit bus mode is used, 2-line display mode is used, and 5x8 dot display format is used
-// 	ADM0802A_CommandWrite (ADM0802A_COMMAND_FUNCTION_SET (1, 1, 0));
+		#ifdef GPIOM
+			case 'M':	return PORT (M);
+		#endif
 
-// 	// Clear Display and return to home
-// 	ADM0802A_CommandWrite (ADM0802A_COMMAND_CLEAR_DISPLAY);
+		#ifdef GPION
+			case 'N':	return PORT (N);
+		#endif
 
-// 	return 0;
-// }
+		#ifdef GPIOO
+			case 'O':	return PORT (O);
+		#endif
 
-// /*
-//  * COMMAND FUNCTIONS
-//  */
+		#ifdef GPIOP
+			case 'P':	return PORT (P);
+		#endif
 
-// void ADM0802A_CommandWrite (uint8_t command) {
-// 	// Write the command to the data bus
-// 	HAL_GPIO_WritePin (Port_DB0, Pin_DB0,  command & 0x01);
-// 	HAL_GPIO_WritePin (Port_DB1, Pin_DB1, (command >> 1) & 0x01);
-// 	HAL_GPIO_WritePin (Port_DB2, Pin_DB2, (command >> 2) & 0x01);
-// 	HAL_GPIO_WritePin (Port_DB3, Pin_DB3, (command >> 3) & 0x01);
-// 	HAL_GPIO_WritePin (Port_DB4, Pin_DB4, (command >> 4) & 0x01);
-// 	HAL_GPIO_WritePin (Port_DB5, Pin_DB5, (command >> 5) & 0x01);
-// 	HAL_GPIO_WritePin (Port_DB6, Pin_DB6, (command >> 6) & 0x01);
-// 	HAL_GPIO_WritePin (Port_DB7, Pin_DB7, (command >> 7) & 0x01);
+			default: 	return PORT (A);
+	}
+}
 
-// 	// Toggle Enable pin to let device receive the information
-// 	HAL_GPIO_WritePin (Port_E, Pin_E, GPIO_PIN_RESET);
-// 	HAL_Delay (1);
-// 	HAL_GPIO_WritePin (Port_E, Pin_E, GPIO_PIN_SET);
-// 	HAL_Delay (1);
-// 	HAL_GPIO_WritePin (Port_E, Pin_E, GPIO_PIN_RESET);
+static uint16_t GetPin (char* pin) {
+	switch (atoi (pin)) {
+		#ifdef GPIO_PIN_0
+			case 0:		return PIN (0);
+		#endif
 
-// 	// Delay to let instruction be executed according to Instruction table p.8
-// 	if (command >> 7)			HAL_Delay (12);
-// 	else if (command >> 6) 		HAL_Delay (12);
-// 	else if (command >> 5) 		HAL_Delay (12);
-// 	else if (command >> 4) 		HAL_Delay (12);
-// 	else if (command >> 3) 		HAL_Delay (12);
-// 	else if (command >> 2) 		HAL_Delay (12);
-// 	else if (command >> 1) 		HAL_Delay (46);
-// 	else						HAL_Delay (46);
-// }
+		#ifdef GPIO_PIN_1
+			case 1:		return PIN (1);
+		#endif
 
-// /*
-//  * DATA FUNCTIONS
-//  */
+		#ifdef GPIO_PIN_2
+			case 2:		return PIN (2);
+		#endif
 
-// void ADM0802A_DataWrite (char character, bool upper_set) {
-// 	// Toggle RS pin to high to enable writing to RAM
-// 	HAL_GPIO_WritePin (Port_RS, Pin_RS, GPIO_PIN_SET);
+		#ifdef GPIO_PIN_3
+			case 3:		return PIN (3);
+		#endif
 
-// 	// Translate the character into the encoding from p.9
-// 	uint8_t value = CharToEncoding (character, 0);
+		#ifdef GPIO_PIN_4
+			case 4:		return PIN (4);
+		#endif
 
-// 	// Writ the value to the data bus
-// 	HAL_GPIO_WritePin (Port_DB0, Pin_DB0,  value & 0x01);
-// 	HAL_GPIO_WritePin (Port_DB1, Pin_DB1, (value >> 1) & 0x01);
-// 	HAL_GPIO_WritePin (Port_DB2, Pin_DB2, (value >> 2) & 0x01);
-// 	HAL_GPIO_WritePin (Port_DB3, Pin_DB3, (value >> 3) & 0x01);
-// 	HAL_GPIO_WritePin (Port_DB4, Pin_DB4, (value >> 4) & 0x01);
-// 	HAL_GPIO_WritePin (Port_DB5, Pin_DB5, (value >> 5) & 0x01);
-// 	HAL_GPIO_WritePin (Port_DB6, Pin_DB6, (value >> 6) & 0x01);
-// 	HAL_GPIO_WritePin (Port_DB7, Pin_DB7, (value >> 7) & 0x01);
+		#ifdef GPIO_PIN_5
+			case 5:		return PIN (5);
+		#endif
 
-// 	// Toggle Enable pin to let device receive the information
-// 	HAL_GPIO_WritePin (Port_E, Pin_E, GPIO_PIN_RESET);
-// 	HAL_Delay (1);
-// 	HAL_GPIO_WritePin (Port_E, Pin_E, GPIO_PIN_SET);
-// 	HAL_Delay (1);
-// 	HAL_GPIO_WritePin (Port_E, Pin_E, GPIO_PIN_RESET);
+		#ifdef GPIO_PIN_6
+			case 6:		return PIN (6);
+		#endif
 
-// 	// Delay to let instruction be executed according to Instruction table p.8
-// 	HAL_Delay (12);
+		#ifdef GPIO_PIN_7
+			case 7:		return PIN (7);
+		#endif
 
-// 	// Toggle RS pin to low
-// 	HAL_GPIO_TogglePin (Port_RS, Pin_RS);
-// }
+		#ifdef GPIO_PIN_8
+			case 8:		return PIN (8);
+		#endif
 
-// static inline uint8_t CharToEncoding (char character, bool upper_set) {
-// 	return (upper_set) ? (0x80 | character) : (character);
-// }
+		#ifdef GPIO_PIN_9
+			case 9:		return PIN (9);
+		#endif
+
+		#ifdef GPIO_PIN_10
+			case 10:	return PIN (10);
+		#endif
+
+		#ifdef GPIO_PIN_11
+			case 11:	return PIN (11);
+		#endif
+
+		#ifdef GPIO_PIN_12
+			case 12:	return PIN (12);
+		#endif
+
+		#ifdef GPIO_PIN_13
+			case 13:	return PIN (13);
+		#endif
+
+		#ifdef GPIO_PIN_14
+			case 14:	return PIN (14);
+		#endif
+
+		#ifdef GPIO_PIN_15
+			case 15:	return PIN (15);
+		#endif
+
+			default: 	return PIN (0);
+	}
+}
+
+
+
--- ADM0802A.h
+++ ADM0802A.h
@@ -12,12 +12,50 @@
 #define INC_ADM0802A_H_
 
 #include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
-#include "stm32f3xx_hal.h"
+#include "stm32f3xx_hal.h" // Change this line to your stm32 mcu model
 
 /*
- * GPIO PIN NAMES
+ * MACROS
  */
+#define PIN_PAIR(x, y) (#x#y)
+#define CAT(x, ...) (x ## __VA_ARGS__)
+#define PORT(x) (CAT (GPIO, x))
+#define PIN(x) (CAT (GPIO_PIN_, x))
+
+/*
+ * GPIO PIN VARIABLES
+ */
+
+static GPIO_TypeDef* port_rw;
+static GPIO_TypeDef* port_rs;
+static GPIO_TypeDef* port_ca;
+static GPIO_TypeDef* port_e;
+static GPIO_TypeDef* port_db0;
+static GPIO_TypeDef* port_db1;
+static GPIO_TypeDef* port_db2;
+static GPIO_TypeDef* port_db3;
+static GPIO_TypeDef* port_db4;
+static GPIO_TypeDef* port_db5;
+static GPIO_TypeDef* port_db6;
+static GPIO_TypeDef* port_db7;
+
+static uint16_t pin_rw;
+static uint16_t pin_rs;
+static uint16_t pin_ca;
+static uint16_t pin_e;
+static uint16_t pin_db0;
+static uint16_t pin_db1;
+static uint16_t pin_db2;
+static uint16_t pin_db3;
+static uint16_t pin_db4;
+static uint16_t pin_db5;
+static uint16_t pin_db6;
+static uint16_t pin_db7;
+
 
 #define Pin_RW GPIO_PIN_0
 #define Port_RW GPIOA
@@ -61,7 +99,7 @@
  * INITIALIZATION
  */
 
-uint8_t ADM0802A_Init ();
+uint8_t ADM0802A_Init (char* rw_pair, char* rs_pair, char* ca_pair, char* e_pair, char* db0_pair, char* db1_pair, char* db2_pair, char* db3_pair, char* db4_pair, char* db5_pair, char* db6_pair, char* db7_pair);
 
 /*
  * COMMAND FUNCTIONS
@@ -75,85 +113,5 @@ void ADM0802A_CommandWrite (uint8_t command);
 
 void ADM0802A_DataWrite (char character, bool upper_set);
 
+
 #endif /* INC_ADM0802A_H_ */
-
-
-// /*
-//  * ADM0802A.h
-//  *
-//  *  Created on: Feb 22, 2025
-//  *      Author: josue
-//  *
-//  *  Based of "ADM0802A-FSY-YBS-3.3V.doc" From cdn.sparkfun.com
-//  *  https://cdn.sparkfun.com/datasheets/LCD/Monochrome/ADM0802A-FSY-YBS-3.3V.pdf
-//  */
-
-// #ifndef INC_ADM0802A_H_
-// #define INC_ADM0802A_H_
-
-// #include <stdbool.h>
-// #include <stdarg.h>
-
-// #include "stm32f3xx_hal.h"
-
-// /*
-//  * GPIO PIN NAMES
-//  */
-
-// #define Pin_RW GPIO_PIN_0
-// #define Port_RW GPIOA
-// #define Pin_RS GPIO_PIN_1
-// #define Port_RS GPIOA
-// #define Pin_CA GPIO_PIN_4
-// #define Port_CA GPIOA
-// #define Pin_E GPIO_PIN_5
-// #define Port_E GPIOA
-// #define Pin_DB0 GPIO_PIN_6
-// #define Port_DB0 GPIOA
-// #define Pin_DB1 GPIO_PIN_7
-// #define Port_DB1 GPIOA
-// #define Pin_DB2 GPIO_PIN_0
-// #define Port_DB2 GPIOB
-// #define Pin_DB3 GPIO_PIN_1
-// #define Port_DB3 GPIOB
-// #define Pin_DB4 GPIO_PIN_2
-// #define Port_DB4 GPIOB
-// #define Pin_DB5 GPIO_PIN_10
-// #define Port_DB5 GPIOB
-// #define Pin_DB6 GPIO_PIN_4
-// #define Port_DB6 GPIOC
-// #define Pin_DB7 GPIO_PIN_5
-// #define Port_DB7 GPIOC
-
-// /*
-//  * COMMAND SHORTCUTS
-//  */
-
-// #define ADM0802A_COMMAND_CLEAR_DISPLAY 					 0x01
-// #define ADM0802A_COMMAND_RETURN_HOME 					 0x02
-// #define ADM0802A_COMMAND_ENTRY_MODE(ID, SH)				(0x04 | ((ID != 0) << 1) | (SH != 0))
-// #define ADM0802A_COMMAND_DISPLAY_CONTROL(D, C, B)		(0x08 | ((D != 0) << 2) | ((C != 0) << 1) | (B != 0))
-// #define ADM0802_COMMAND_CURSOR_DISPLAY_SHIFT(SC, RL)	(0x10 | ((SC != 0) << 3) | ((RL != 0) << 2))
-// #define ADM0802A_COMMAND_FUNCTION_SET(DL, N, F)			(0x20 | ((DL != 0) << 4) | ((N != 0) << 3) | ((F != 0) << 2))
-// #define ADM0802A_COMMAND_SET_CGRAM_ADDR(AC) 			(0x40 | (AC & 0x3F))
-// #define ADM0802A_COMMAND_SET_DDRAM_ADDR(AC) 			(0x80 | (AC & 0x7F)) // ONLY FROM 0x00 to 0x4F is in 1 line display mode, other from 0x00 to 0x27 for line 1 and 0x40 to 0x67 for line 2
-
-// /*
-//  * INITIALIZATION
-//  */
-
-// uint8_t ADM0802A_Init (uint8_t num_pins_to_set, ...);
-
-// /*
-//  * COMMAND FUNCTIONS
-//  */
-
-// void ADM0802A_CommandWrite (uint8_t command);
-
-// /*
-//  * DATA FUNCTIONS
-//  */
-
-// void ADM0802A_DataWrite (char character, bool upper_set);
-
-// #endif /* INC_ADM0802A_H_ */
